724a6c5340fa3baafcd9b7e6ebc2bc3ad3ca828a
Orago <34244531+Orago@users.noreply.github.com>
2020-07-03 12:57:59 -0700

    Update discord-config.js

--- discord-config.js
+++ discord-config.js
@@ -11,10 +11,10 @@ const config = convict({
     infoCommand: {
         doc: "The command which the bot will respond to with the site URL.",
         format: String,
-        default: "!d-zone"
+        default: "!mittz-zone"
     },
     autoPopulate: {
-        doc: "Whether to create a d-zone for each Discord server the bot is in.",
+        doc: "Whether to create a mittz-zone for each Discord server the bot is in.",
         format: Boolean,
         default: true
     },
@@ -23,7 +23,7 @@ const config = convict({
             id: {
                 doc: "The Discord server ID you want to simulate.",
                 format: String,
-                default: "123456789"
+                default: "396838715894530068"
             },
             default: {
                 doc: "Indicates whether clients connect to this server by default. One server should have this set to true.",
